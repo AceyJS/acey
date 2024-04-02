9f0ef34fecaac3125b3d597c0bfb60373d4d0680
fantasim <siuol@gmx.com>
2024-04-02 07:11:44 +0300

    avoid concurrentt writtin

--- src/model/local-store.ts
+++ src/model/local-store.ts
@@ -8,7 +8,7 @@ export default class LocalStoreManager {
     constructor(m: Model){
         this._m = m
     }
-
+    private _isWritting = false
     private _model = (): Model => this._m
     public isActive = (): boolean => Manager.isInitialized() && !Config.isNextJSServer() && !this._model().is().keyGenerated() && this._model().is().connected() && Manager.localStoreManager() != null
 
@@ -23,11 +23,19 @@ export default class LocalStoreManager {
         return undefined
     }
 
-    public set = (expires = 365) => {
+    public set = (expires = 365): Promise<void> => {
         this._throwErrorIfInactive()
+        if (this._isWritting)
+            return Promise.resolve()
+        
         const key = this._model().super().option().key()
         try {
-            return Manager.localStoreManager().addElement(key, this._model().to().locallyStorableString(), expires)
+            this._isWritting = true
+            const p = Manager.localStoreManager().addElement(key, this._model().to().locallyStorableString(), expires)
+            p.then(() => this._isWritting = false)
+            return new Promise((resolve, reject) => {
+                p.then(resolve).catch(reject)
+            }) 
         } catch (e) {
             throw new Error(`error from localStore set with ${this._model().is().collection() ? 'Collection' : 'Model'}: ${key}, ${e}`)
         }
